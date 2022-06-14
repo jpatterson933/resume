9bc7238783a5896ae74ce5e36f1069e7942f8f1c
Jeffery Patterson <jpatterson933@ucla.edu>
2022-06-13 18:57:20 -0700

    finalized calculateExperience in class languages

--- language.js
+++ language.js
@@ -1,17 +1,51 @@
 
+// these three const get our current year and our current month strings
+const date = new Date().toISOString();
+const curYr = parseInt(date.split("-")[0]);
 
-const date = new Date();
-
-const languages = [
-    {
-        languageName: "HTML",
-        yearStarted: new Date(2021, 1),
-        currentYear: new Date()
+console.log(curYr)
+class Language {
+    constructor(name, startYear, currentYear, projects) {
+        this.name = name;
+        this.startYear = startYear;
+        this.currentYear = currentYear;
+        this.projects = projects;
     }
-]
-const languageCard = document.getElementById('languages');
 
-console.log(languages[0].yearStarted)
-console.log(languages[0].currentYear)
+    // function to calculate total experience for years if years are the same experience will return as 0
+    calculateExperience() {
+
+        if (this.currentYear === this.startYear) {
+            return 0;
+        }
+        let totalYrExp = this.currentYear - this.startYear;
+
+        return totalYrExp;
+
+    }
+};
+
+const html = new Language('HTML', 2021, curYr, 4);
+
+console.log(html.startYear)
+
+
+const languageCard = $("#languages");
+
+const languageCardDisplay = (appendTo) => {
+    card = `<div id="language-card-wrapper">
+            <h1>${html.name}</h1>
+            <p>Started in ${html.startYear}</p>
+            <p>Current Year: ${html.currentYear}</p>
+            <p>Total Experience: ${html.calculateExperience()} year</p>
+        </div>
+    `
+
+    appendTo.append(card);
+};
+
+languageCardDisplay(languageCard);
+
+
 
 
